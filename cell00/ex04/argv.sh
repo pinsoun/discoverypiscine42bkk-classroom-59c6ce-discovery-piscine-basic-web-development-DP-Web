b9ca0e3ccbacbffbbfab3e85787d81ef783d7eb8
pinsoun <pear.pearpp1234@gmail.com>
2025-06-18 10:21:13 +0700

    0004

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,16 @@
+if [ $# -eq 0 ]
+then
+	echo "No arguments supplied";
+elif [ $# -eq 1 ]
+then
+	echo $1
+elif [ $# -eq 2 ] 
+then
+	echo $1
+	echo $2
+elif [ $# -eq 3 ]
+then
+	echo $1
+	echo $2
+	echo $3
+fi
